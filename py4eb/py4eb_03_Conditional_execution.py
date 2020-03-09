b774c428d4088707107272361d111fe391fb2d0c
edu4u-games <59312731+edu4u-games@users.noreply.github.com>
2020-03-09 11:57:49 -0300

    finished py4eb_03 and begin py4eb_04

--- py4eb/py4eb_03_Conditional_execution.py
+++ py4eb/py4eb_03_Conditional_execution.py
@@ -1,5 +1,3 @@
-
-
 '''
 Boolean Expressions
 A boolean expression is an expression that is either true or false.
@@ -13,12 +11,12 @@ x <= y # x is less than or equal to y
 x is y # x is the same as y
 x is not y # x is not the same as y
 '''
-assert 5==5
-assert 4!=6
+assert 5 == 5
+assert 4 != 6
 assert type(True), 'bool'
-x= 5
-y= 5
-z= 8
+x = 5
+y = 5
+z = 8
 assert x is y
 assert x is not z
 
@@ -34,9 +32,58 @@ assert x % 2 == 0 or z % 2 == 0
 assert not (x > z)
 assert 21 and True
 
-
 '''
 Conditional Execution
+if - elif - else
+'''
+if x < y:
+    print('x is less than y')
+elif x > y:
+    print('x is greater than y')
+else:
+    print('x and y are equal')
 
 '''
+Catching exceptions Using Try and Except
+The idea of try and except is that you know that some sequence of instruction(s) may have a problem and
+you want to add some statements to be executed if an error occurs. These extra statements (the except block) are ignored if
+there is no error.
 
+inp = input('Enter Fahrenheit Temperature:')
+try:
+    fahr = float(inp)
+    cel = (fahr - 32.0) * 5.0 / 9.0
+    print(cel)
+except:
+    print('Please enter a number')
+'''
+
+'''
+Short-Circuit Evaluation of Logical Expressions
+When Python is processing a logical expression such as x >= 2 and (x/y) > 2 , it evaluates the expression from left
+to right. Because of the definition of and , if x is less than 2, the expression x >= 2 is False and so the whole
+expression is False regardless of whether (x/y) > 2 evaluates to True or False .
+When the evaluation of a logical expression stops because
+the overall value is already known, it is called short-circuiting the evaluation,  the short-circuit behavior leads to
+a clever technique called the guardian pattern, for example preventing a zero division, like the code:
+x >= 2 and y != 0 and (x/y) > 2
+If y == 0 then the term (x/y) is not executed.
+'''
+'''
+Exercise 1: Rewrite your pay computation to give the employee 1.5 times the hourly rate
+ for hours worked above 40 hours and prevet it for ilegal imput. 
+'''
+while True:
+    name = input("Name: ")
+    try:
+        hours = float(input("Hours: "))
+        rate = float(input("Rate: "))
+    except:
+        print("Please enter a number")
+    if hours > 40:
+        extra_hours = hours - 40
+        print("Pay ", 40 * rate + extra_hours * rate * 1.5, ' $ for ', name)
+    else:
+        print("Pay ", hours * rate, ' $ for ', name)
+    if input("Type 'S' to exit.") == 'S':
+        break
